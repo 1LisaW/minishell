b9f4b7c2db415a0c35ebab8874408502480bfad4
root <root@LAPTOP-4954DC8J>
2023-10-12 11:35:09 +0300

    feat: add function for launching  execution of buildin cmds

--- src/executor/execute_process.c
+++ src/executor/execute_process.c
@@ -6,12 +6,22 @@
 /*   By: tklimova <tklimova@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:51:15 by tklimova          #+#    #+#             */
-/*   Updated: 2023/10/09 13:04:49 by tklimova         ###   ########.fr       */
+/*   Updated: 2023/10/12 11:31:21 by tklimova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/mini_shell.h"
 
+int	run_buildin(t_parser_data *parser_node, int opt)
+{
+	if ((parser_node->flags & IS_PIPE) == opt
+		&& !ft_strcmp(parser_node->text, "/bin/pwd"))
+	{
+		printf("We should run buildin command!!! flags:%d\n", parser_node->flags);
+		return (1);
+	}
+	return (0);
+}
 
 int	child_process(int *prev_fd, int *fd, t_parser_data *parser_node)
 {
@@ -24,7 +34,8 @@ int	child_process(int *prev_fd, int *fd, t_parser_data *parser_node)
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
 	}
-
+	if (run_buildin(parser_node, 0x2))
+		return (0);
 	execve(parser_node->text, parser_node->cmd_line, NULL);
 	write(1, "\n has error \n", 10);
 	return (4);
@@ -35,6 +46,8 @@ int	create_process(int *prev_fd, t_parser_data *parser_node)
 	int	fd[2];
 	int	child_id;
 
+	if (run_buildin(parser_node, 0x0))
+		return (0);
 	if (!(parser_node->flags & IS_WAIT) && pipe(fd) == -1)
 		return (1);
 	child_id = fork();
@@ -55,6 +68,8 @@ int	create_process(int *prev_fd, t_parser_data *parser_node)
 
 void	execute_process(int *prev_fd, t_parser_data *parser_node)
 {
+	if (parser_node->lexer_type == operator)
+		return ;
 	if (parser_node->lexer_type == word)
 		create_process(prev_fd, parser_node);
 	if (parser_node->flags & IS_WAIT)
